53b189cd569011ff3992277ccbf2bebc04163e0f
yoonseoq <sysooo18@naver.com>
2024-11-04 01:50:59 +0900

    1103

--- src/main/java/com/seok/_4/code_60_1.java
+++ src/main/java/com/seok/_4/code_60_1.java
@@ -2,18 +2,15 @@ package com.seok._4;
 
 public class code_60_1 {
     public static void main(String[] args) {
-        int n = 9;
-        boolean is369 =false;
+        int n = 10;
         for (int i = 1; i <= n; i++) {
-            while (i > 0) {
-                if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
-                    is369 = true;
-                    System.out.println("Wkr");
-                    continue;
-                }
-                i /= 10;
+            if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+
+                System.out.print(" 짝");
+                continue;
             }
-                System.out.println(i);
+            System.out.print(" "+i);
         }
     }
+
 }
